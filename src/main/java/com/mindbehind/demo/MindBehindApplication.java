f8db083297b14df2e8687940ae8e0f5bd2033a5b
semihunaldi <semihunaldi@gmail.com>
2021-06-29 15:59:47 +0300

    remove unnecessary annotation

--- src/main/java/com/mindbehind/demo/MindBehindApplication.java
+++ src/main/java/com/mindbehind/demo/MindBehindApplication.java
@@ -1,11 +1,9 @@
 package com.mindbehind.demo;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class MindBehindApplication {
 
     public static void main(String[] args) {
